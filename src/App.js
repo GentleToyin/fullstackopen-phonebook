ae40705fd555e8523c3c940bebc896b5aa05ae4b
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2021-06-24 10:58:15 +0100

    created phonebook to display name input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+ 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas',
+      number:'0801111111'
+    } 
+
+  ]) 
+
+  const handleNameChange=(event)=>{
+    setNewName(event.target.value)
+  }
+   const handleNumberChange=(event)=>{
+    setNewNumber(event.target.value)
+  }
+ 
+  const addName=(event)=>{
+    event.preventDefault()
+    if(filter){
+        console.log(`${newName} is already there`)
+      }
+    else{
+      const newNameObject ={
+          name:newName,
+          number:newNumber
+        }
+        setPersons(persons.concat(newNameObject))
+        setNewName('')
+    }  
+  }
+
+ 
+  const nameList = persons.map(person=> <ul>{person.name} {person.number}</ul>)
+  const filter = persons.find(obj => obj.name===newName);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input
+          value = {newName}
+          onChange={handleNameChange} />
+        </div>
+         <div>
+          number: <input
+          value = {newNumber}
+          onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button onClick= {addName} type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+
+        {nameList}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
